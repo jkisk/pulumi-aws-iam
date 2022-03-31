9fda0f4f186bf9e26d49938d3f0e345766eb49da
Jamie Kinkead <jamie@pulumi.com>
2022-03-31 13:04:13 -0700

    Add role example

---
+++ examples/typescript/role/index.ts
@@ -0,0 +1,19 @@
+import * as iam from "@jkisk/pulumi-aws-iam";
+
+const testRole = new iam.AssumableRole("test-role", {
+    assumeRolePolicy: JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [{
+            Action: "sts:AssumeRole",
+            Effect: "Allow",
+            Sid: "",
+            Principal: {
+                Service: "ec2.amazonaws.com",
+            },
+        }],
+    }),
+    attachReadOnlyPolicy: true
+});
+
+export const arn = testRole.arn;
+export const policies = testRole.policiesAttached;
